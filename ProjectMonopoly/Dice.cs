4747556407f23b9933f6c0a81d37844ec8aaa0c2
sebastien78000 <60258199+sebastien78000@users.noreply.github.com>
2021-12-30 16:27:42 +0100

    Revert "Comments added"
    This reverts commit f0e5dfdb0b88b42db671e93524d33cd973a50575.

--- ProjectMonopoly/Dice.cs
+++ ProjectMonopoly/Dice.cs
@@ -11,23 +11,13 @@ namespace ProjectMonopoly
         static private Random dice = new Random();
         static int[] value = new int[2];
 
-        /// <summary>
-        /// Function to change the value of the dices
-        /// </summary>
         static public void Roll()
         {
             value[0] = dice.Next(1, 7);
             value[1] = dice.Next(1, 7);
         }
 
-        /// <summary>
-        /// Property in read only of attribute value containing the value of the two dices
-        /// </summary>
         static public int[] Value { get => value; }
-
-        /// <summary>
-        /// Property in read only to get a bool if the two dices are equal
-        /// </summary>
         static public bool SameVal { get => Dice.Value[0] == Dice.Value[1]; }
 
     }
--- ProjectMonopoly/State.cs
+++ ProjectMonopoly/State.cs
@@ -11,26 +11,17 @@ namespace ProjectMonopoly
         protected int counter;
         protected Player p;
 
-        /// <summary>
-        /// Access property of the variable p (read and write)
-        /// </summary>
         public Player P
         {
             get { return p; }
             set { p = value; }
         }
-        /// <summary>
-        ///  Access property of the variable counter (read and write)
-        /// </summary>
         public int Counter
         {
             get { return counter; }
             set { counter = value; }
         }
 
-        /// <summary>
-        /// Function to move (mandatory in the two classes stateJail and stateFree)
-        /// </summary>
         public abstract void Move();
     }
 }
