d15b40e4a08438cded104e35187902ad02b88218
sebastien78000 <60258199+sebastien78000@users.noreply.github.com>
2022-01-02 15:28:35 +0100

    added comments dice

--- ProjectMonopoly/Dice.cs
+++ ProjectMonopoly/Dice.cs
@@ -6,19 +6,29 @@ using System.Threading.Tasks;
 
 namespace ProjectMonopoly
 {
-    
+
     public class Dice
     {
         static private Random dice = new Random();
         static int[] value = new int[2];
 
+        /// <summary>
+        /// Function to change the value of the dices
+        /// </summary>
         static public void Roll()
         {
             value[0] = dice.Next(1, 7);
             value[1] = dice.Next(1, 7);
         }
 
+        /// <summary>
+        /// Property in read only of attribute value containing the value of the two dices
+        /// </summary>
         static public int[] Value { get => value; }
+
+        /// <summary>
+        /// Property in read only to get a bool if the two dices are equal
+        /// </summary>
         static public bool SameVal { get => Dice.Value[0] == Dice.Value[1]; }
 
     }
