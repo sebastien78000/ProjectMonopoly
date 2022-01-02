e0d340dd6d2232d7f7c7fde7ccdc7db758f061f6
sebastien78000 <60258199+sebastien78000@users.noreply.github.com>
2022-01-02 15:36:31 +0100

    comments added

--- ProjectMonopoly/End.xaml.cs
+++ ProjectMonopoly/End.xaml.cs
@@ -20,12 +20,21 @@ namespace ProjectMonopoly
     /// </summary>
     public partial class End : Page
     {
+        /// <summary>
+        /// Constructor of the window End
+        /// Has the name of player who won the game as an entry
+        /// </summary>
+        /// <param name="player"></param>
         public End(string player)
         {
             InitializeComponent();
             UpdateMessage(player);
         }
 
+        /// <summary>
+        /// update the box EndMessage to indicate which player has won
+        /// </summary>
+        /// <param name="player"></param>
         public void UpdateMessage(string player)
         {
             EndMessage.Text = $"Player {player} has won the game. Congratulations!";
